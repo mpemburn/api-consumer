da7f902a90507f6c34bba7275b61169aa8cce44f
Mark Pemburn <mark@pemburn.com>
2020-12-01 15:05:51 -0500

    Add exception handling to RequestHandler

--- src/Handlers/RequestHandler.php
+++ src/Handlers/RequestHandler.php
@@ -9,18 +9,27 @@ use Mpemburn\ApiConsumer\Interfaces\EndpointInterface;
 
 class RequestHandler
 {
+    protected $response;
 
     public function send(EndpointInterface $endpoint): self
     {
         try {
-            $response = Http::withHeaders($endpoint->getHeaders())
-                ->withBasicAuth($endpoint->getUsername(), $endpoint->getPassword())
-                ->send($endpoint->getRequestType(), $endpoint->getEndpoint());
-        } catch (RequestException $exception) {
+            $httpClient = Http::withHeaders($endpoint->getHeaders());
+            if ($endpoint->hasBasicAuth()) {
+                $httpClient->withBasicAuth($endpoint->getUsername(), $endpoint->getPassword());
+            }
+
+            $this->response = $httpClient->send($endpoint->getRequestType(), $endpoint->getEndpoint());
 
         } catch (Exception $e) {
+            $this->response = 'Error: ' . $endpoint->getRequestName() . ' responded with ' . $e->getMessage();
         }
 
         return $this;
+    }
+
+    public function getResponse(): string
+    {
+        return $this->response->body();
     }
 }
